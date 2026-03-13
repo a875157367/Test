77b834d605611bda2f3a4a9e52a755052d871251
a875157367 <zhangyunso@126.com>
2026-03-14 05:51:31 +0800

    2

--- test2.py
+++ test2.py
@@ -600,6 +600,7 @@ def actionM3u():
         "https://raw.githubusercontent.com/kakaxi-1/IPTV/main/ipv4.txt",
         "https://ghfast.top/raw.githubusercontent.com/TianmuTNT/iptv/main/iptv.m3u",
         # "http://106.53.99.30/2025.txt",
+        "https://live.zbds.top/tv/iptv4.txt",
         "https://raw.githubusercontent.com/Heiwk/iptv67/refs/heads/main/iptv.m3u",
     ]
     
